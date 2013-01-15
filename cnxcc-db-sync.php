3f822bc26ff8f0dbbc676638655f552d76ebd572
Carlos Ruiz Díaz <carlos.ruizdiaz@gmail.com>
2013-01-15 16:23:28 -0300

    added usage function

--- cnxcc-db-sync.php
+++ cnxcc-db-sync.php
@@ -7,7 +7,10 @@ require_once 'ActiveClient.class.php';
 function main()
 {
 	global $argv, $mysql_params;
-	
+
+	if (count($argv) != 3)
+		show_usage();
+		
 	$serverIP	= $argv[1];
 	$serverPort	= $argv[2];
 	$url		= "http://$serverIP:$serverPort";
@@ -15,6 +18,11 @@ function main()
 	$clients	= new ActiveClients($url, $mysql_params);
 	
 	$clients->retrieve();
+}
+
+function show_usage()
+{
+	die("Invalid parameters:\n\tphp cnxcc-db-sync.php <sip-server-ip> <sip-server-xmlrpc-port>\n");
 }
 
 main();
